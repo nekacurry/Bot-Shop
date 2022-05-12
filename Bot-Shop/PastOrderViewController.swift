0c6ed547078f786c686a9a26f45af0b7326febb8
nekacurry <anneka498@gmail.com>
2022-05-12 15:03:01 -0500

    past order setup

--- Bot-Shop/PastOrderViewController.swift
+++ Bot-Shop/PastOrderViewController.swift
@@ -9,16 +9,7 @@ import Foundation
 import UIKit
 
 class PastOrderViewController: UIViewController {
-    
-    let orders = [
-        Order(title: "July 2020", image: UIImage(named: "box")!),
-        Order(title: "June 2020", image: UIImage(named: "box")!),
-        Order(title: "May 2020", image: UIImage(named: "box")!),
-        Order(title: "December 2019", image: UIImage(named: "box")!),
-        Order(title: "November 2019", image: UIImage(named: "box")!),
-        Order(title: "October 2019", image: UIImage(named: "box")!),
-        Order(title: "September 2019", image: UIImage(named: "box")!)]
-
+    var orders: [Order] = []
     let tableView =  UITableView()
 
     override func viewDidLoad() {
@@ -29,6 +20,7 @@ class PastOrderViewController: UIViewController {
         tableView.delegate = self
         tableView.dataSource = self
         setUpTableView()
+        getItems()
     }
 
     func setUpTableView(){
@@ -38,6 +30,27 @@ class PastOrderViewController: UIViewController {
         tableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
         tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
         tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        
+    }
+    
+    func getItems(){
+        let robot1 = Item(title: "Respiratory", image: UIImage(named: "robot1")!)
+        let robot2 = Item(title: "Muscular", image: UIImage(named: "robot2")!)
+        let robot3 = Item(title: "Endocrine", image: UIImage(named: "robot3")!)
+        let robot4 = Item(title: "Excretory", image: UIImage(named: "robot4")!)
+        let robot5 = Item(title: "Lymphatic", image: UIImage(named: "robot5")!)
+        let robot6 = Item(title: "Nervous", image: UIImage(named: "robot6")!)
+
+        let ordersList = [
+            Order(title: "July 2020", image: UIImage(named: "box")!, items: [robot1, robot3]),
+            Order(title: "June 2020", image: UIImage(named: "box")!, items: [robot2, robot3, robot6]),
+            Order(title: "May 2020", image: UIImage(named: "box")!, items: [robot4, robot1]),
+            Order(title: "December 2019", image: UIImage(named: "box")!, items: [robot2, robot5, robot6])
+        ]
+        
+        for box in ordersList {
+            orders.append(box)
+        }
     }
 
 }
@@ -51,10 +64,10 @@ extension PastOrderViewController: UITableViewDataSource, UITableViewDelegate {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! PastOrderCell
             cell.accessoryType = .disclosureIndicator
             cell.selectionStyle = .none
-        cell.setCellContents(item: orders[indexPath.row])
+        cell.textLabel?.text = "\(indexPath.row + 1) \(orders[indexPath.row].title)"
+        cell.setBoxContents(box: orders[indexPath.row])
         return cell
     }
-    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 100.0
     }
@@ -62,7 +75,9 @@ extension PastOrderViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print("selected!")
         let nextVC: OrderList = OrderList()
+        nextVC.currentOrder = orders[indexPath.row]
         self.navigationController?.pushViewController(nextVC, animated: true)
     }
+
 
 }

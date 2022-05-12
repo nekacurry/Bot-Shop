248c77bc164b873671774022baa9c074492b5bd1
nekacurry <anneka498@gmail.com>
2022-05-12 13:53:09 -0500

    added tabbar

---
+++ Bot-Shop/PastOrderViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PastOrderViewController.swift
+//  Bot-Shop
+//
+//  Created by Anneka Curry on 5/12/22.
+//
+
+import Foundation
+import UIKit
+
+class PastOrderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .green
+    }
+
+}
---
+++ Bot-Shop/TabBarController.swift
@@ -0,0 +1,50 @@
+//
+//  TabBarController.swift
+//  Bot-Shop
+//
+//  Created by Anneka Curry on 5/12/22.
+//
+
+import Foundation
+import UIKit
+
+class TabBarController: UITabBarController, UITabBarControllerDelegate{
+    
+    var firstItemImageView: UIImageView!
+    var secondItemImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.delegate = self
+        setupViewControllers()
+        setupTabBarIcons()
+    }
+    
+    func setupTabBarIcons(){
+
+        let firstItemView = self.tabBar.subviews[0]
+        let secondItemView = self.tabBar.subviews[1]
+
+        self.firstItemImageView = (firstItemView.subviews.first as! UIImageView)
+        self.firstItemImageView.contentMode = .center
+
+        self.secondItemImageView = (secondItemView.subviews.first as! UIImageView)
+        self.secondItemImageView.contentMode = .center
+    }
+    
+    func setupViewControllers(){
+
+        let newBoxVC = NewOrderViewController()
+        let navController1 = UINavigationController(rootViewController:newBoxVC)
+        newBoxVC.tabBarItem = UITabBarItem(title: "New", image: UIImage(named: "tab-box"), tag: 0)
+
+        let pastBoxesVC = PastOrderViewController()
+        let navController2 = UINavigationController(rootViewController:pastBoxesVC)
+        pastBoxesVC.tabBarItem = UITabBarItem(title: "History", image: UIImage(named: "tab-history"), tag: 1)
+
+
+        viewControllers = [navController1, navController2]
+
+    }
+
+}

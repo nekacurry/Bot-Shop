a93b6a635fd59521899eda609e3b062d4d8fa3b4
nekacurry <anneka498@gmail.com>
2022-05-12 14:29:27 -0500

    users can see items

---
+++ Bot-Shop/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Bot-Shop
+//
+//  Created by Anneka Curry on 5/12/22.
+//
+
+import Foundation
+import UIKit
+
+struct Item {
+    var title: String
+    var image: UIImage
+}
